f1034758227b7e00a007d50b6dd93bc687cd662a
Andrew McNutt <mcnutt.andrew@gmail.com>
2019-02-18 11:49:26 -0600

    Implement over operator

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
 
-export function hexToRGB(hex) {
-  const values = hex
-    .replace(/rgba?\(/, '')
-    .replace(/\)/, '')
-    .replace(/[\s+]/g, '')
-    .split(',');
-  return {r: values[0], g: values[1], b: values[0]};
+function hexToRGB(hexIn) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const hex = hexIn.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  if (!result) {
+    return null;
+  }
+  return {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+  };
 }
-
 
 export function rgbToHexCoord(rgb) {
   const hex = Number(rgb).toString(16);
@@ -15,12 +21,12 @@ export function rgbToHexCoord(rgb) {
 }
 
 export function rgbToHex(r, g, b) {
-  return `${rgbToHexCoord(r)}${rgbToHexCoord(g)}${rgbToHexCoord(b)}`;
+  return `#${rgbToHexCoord(r)}${rgbToHexCoord(g)}${rgbToHexCoord(b)}`;
 }
 
 export function computeOver(color, background, alpha) {
   const colorRGB = hexToRGB(color);
   const backgroundRGB = hexToRGB(background);
-  const overRGB = ['r', 'g', 'b'].map(d => colorRGB[d] * alpha + (1 - alpha) * backgroundRGB[d]);
+  const overRGB = ['r', 'g', 'b'].map(d => Math.round(colorRGB[d] * alpha + (1 - alpha) * backgroundRGB[d]));
   return rgbToHex(...overRGB);
 }
--- test/test.js
+++ test/test.js
@@ -2,6 +2,12 @@ const tape = require("tape");
 const {computeOver} = require("../");
 
 tape('Basic test', t => {
-  t.equal(computeOver('#ff0000', '#000000', 0.004), '#01000');
+  t.equal(computeOver('#ff0000', '#000000', 0.004), '#010000');
+  t.equal(computeOver('#f00', '#000000', 0.004), '#010000');
+  t.equal(computeOver('#f00', '#000', 0.004), '#010000');
+
+  t.equal(computeOver('#ff0000', '#ffffff', 0.004), '#fffefe');
+  t.equal(computeOver('#f00', '#ffffff', 0.004), '#fffefe');
+  t.equal(computeOver('#f00', '#fff', 0.004), '#fffefe');
   t.end();
 });
